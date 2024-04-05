2d5931cc8b1c26c2395799016cc267b539c12013
Murilo Brum Sena <165914609+murilo-b-s@users.noreply.github.com>
2024-04-05 12:36:19 -0300

    Atualizar o ex7.py

--- aula lógica programação/exercicios/ex7.py
+++ aula lógica programação/exercicios/ex7.py
@@ -6,20 +6,20 @@ num1 = int(input("Digite o primeiro valor: "))
 num2 = int(input("Digite o segundo valor: "))
 num3 = int(input("Digite o terceiro valor: "))
 
-if num1 > num2 and num1 > num3:
-    if num2 > num3:
+if num1 < num2 and num1 < num3:
+    if num2 < num3:
         print(num1, num2, num3)
     else:
         print(num1, num3, num2)
         
-elif num2 > num1 and num2 > num3:
-    if num1 > num3:
+elif num2 < num1 and num2 < num3:
+    if num1 < num3:
         print(num2, num1, num3)
     else:
         print(num2, num3, num1)
         
-if num3 > num1 and num3 > num2:
-    if num1 > num2:
+if num3 < num1 and num3 < num2:
+    if num1 < num2:
         print(num3, num1, num2)
     else:
         print(num3, num2, num1)
