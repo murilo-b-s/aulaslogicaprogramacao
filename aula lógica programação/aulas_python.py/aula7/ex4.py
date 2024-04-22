f313fdbae42d9b16485dd955c023446705ecf39c
Murilo Brum Sena <165914609+murilo-b-s@users.noreply.github.com>
2024-04-22 16:31:22 -0300

    Add files via upload

--- aula lógica programação/aulas_python.py/aula7/ex4.py
+++ aula lógica programação/aulas_python.py/aula7/ex4.py
@@ -4,3 +4,64 @@
 # Exemplo: 
 # entrada: 10/04/2024
 # saida : 10 de abril de 2024.
+
+
+while True:
+
+    data_digitada = input("Digite uma a data desejada no formato dd/mm/aaaa: ")
+
+    data = data_digitada.split("/")
+    mes = int(data[1])
+    dia = int(data[0])
+
+
+
+    if dia > 31 or dia < 1:
+        print("Data inválida!")
+        continue
+
+    elif mes > 12 or mes < 1:
+        print("Data inválida!")
+        continue
+    
+    else:
+        break
+
+    
+
+if mes == 1:
+    print(f"{data[0]} de Janeiro de {data[2]}")
+    
+if mes == 2:
+    print(f"{data[0]} de Fevereiro de {data[2]}")
+
+if mes == 3:
+    print(f"{data[0]} de Março de {data[2]}")
+
+if mes == 4:
+    print(f"{data[0]} de Abril de {data[2]}")
+
+if mes == 5:
+    print(f"{data[0]} de Maio de {data[2]}")
+
+if mes == 6:
+    print(f"{data[0]} de Junho de {data[2]}")
+
+if mes == 7:
+    print(f"{data[0]} de Julho de {data[2]}")
+
+if mes == 8:
+    print(f"{data[0]} de Agosto de {data[2]}")
+
+if mes == 9:
+    print(f"{data[0]} de Setembro de {data[2]}")
+
+if mes == 10:
+    print(f"{data[0]} de Outubro de {data[2]}")
+
+if mes == 11:
+    print(f"{data[0]} de Novembro de {data[2]}")
+
+if mes == 12:
+    print(f"{data[0]} de Dezembro de {data[2]}")
+
