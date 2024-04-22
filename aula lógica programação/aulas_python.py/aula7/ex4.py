a1ee7c54ccc94aa5b2d8558e444a6ffd40d0bcf7
Murilo Brum Sena <165914609+murilo-b-s@users.noreply.github.com>
2024-04-22 14:28:59 -0300

    Atualizar o ex4.py

--- aula lógica programação/aulas_python.py/aula7/ex4.py
+++ aula lógica programação/aulas_python.py/aula7/ex4.py
@@ -3,9 +3,4 @@
 # e escreva esta com o nome do mês por extenso.
 # Exemplo: 
 # entrada: 10/04/2024
-# saida : 10 de abril de 2024.Exercício 4
-# Faça um algoritmo que leia uma string no formato de data (dd/mm/aaaa)
-# e escreva esta com o nome do mês por extenso.
-# Exemplo: 
-# entrada: 10/04/2024
 # saida : 10 de abril de 2024.
