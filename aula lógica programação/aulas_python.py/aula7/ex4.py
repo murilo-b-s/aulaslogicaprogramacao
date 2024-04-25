6f13d6836d98b5b121ae52ea393969d7349618c2
Murilo Brum Sena <165914609+murilo-b-s@users.noreply.github.com>
2024-04-25 17:35:10 -0300

    Add files via upload

--- aula lógica programação/aulas_python.py/aula7/ex4.py
+++ aula lógica programação/aulas_python.py/aula7/ex4.py
@@ -10,6 +10,7 @@ while True:
 
     data_digitada = input("Digite uma a data desejada no formato dd/mm/aaaa: ")
 
+    meses = [" ", "Janeiro", "Fevereiro", "Março","Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
     data = data_digitada.split("/")
     mes = int(data[1])
     dia = int(data[0])
@@ -28,40 +29,41 @@ while True:
         break
 
     
+print(f"{data[0]} de {meses[mes]} de {data[2]}")
 
-if mes == 1:
-    print(f"{data[0]} de Janeiro de {data[2]}")
+# if mes == 1:
+#     print(f"{data[0]} de {meses[mes]} de {data[2]}")
     
-if mes == 2:
-    print(f"{data[0]} de Fevereiro de {data[2]}")
+# if mes == 2:
+#     print(f"{data[0]} de Fevereiro de {data[2]}")
 
-if mes == 3:
-    print(f"{data[0]} de Março de {data[2]}")
+# if mes == 3:
+#     print(f"{data[0]} de Março de {data[2]}")
 
-if mes == 4:
-    print(f"{data[0]} de Abril de {data[2]}")
+# if mes == 4:
+#     print(f"{data[0]} de Abril de {data[2]}")
 
-if mes == 5:
-    print(f"{data[0]} de Maio de {data[2]}")
+# if mes == 5:
+#     print(f"{data[0]} de Maio de {data[2]}")
 
-if mes == 6:
-    print(f"{data[0]} de Junho de {data[2]}")
+# if mes == 6:
+#     print(f"{data[0]} de Junho de {data[2]}")
 
-if mes == 7:
-    print(f"{data[0]} de Julho de {data[2]}")
+# if mes == 7:
+#     print(f"{data[0]} de Julho de {data[2]}")
 
-if mes == 8:
-    print(f"{data[0]} de Agosto de {data[2]}")
+# if mes == 8:
+#     print(f"{data[0]} de Agosto de {data[2]}")
 
-if mes == 9:
-    print(f"{data[0]} de Setembro de {data[2]}")
+# if mes == 9:
+#     print(f"{data[0]} de Setembro de {data[2]}")
 
-if mes == 10:
-    print(f"{data[0]} de Outubro de {data[2]}")
+# if mes == 10:
+#     print(f"{data[0]} de Outubro de {data[2]}")
 
-if mes == 11:
-    print(f"{data[0]} de Novembro de {data[2]}")
+# if mes == 11:
+#     print(f"{data[0]} de Novembro de {data[2]}")
 
-if mes == 12:
-    print(f"{data[0]} de Dezembro de {data[2]}")
+# if mes == 12:
+#     print(f"{data[0]} de Dezembro de {data[2]}")
 
